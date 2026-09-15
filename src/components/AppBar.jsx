bfec9e9275979850b967b9d23fe40421d557e8d4
Student <student@sirafrica.org>
2026-09-15 14:46:16 +0300

    7. Scrollable app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable, ScrollView } from 'react-native';
 import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
 
@@ -8,6 +8,9 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#24292e',
+  },
+
+  tabs: {
     flexDirection: 'row',
   },
 
@@ -24,13 +27,15 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to="/" component={Pressable} style={styles.tab}>
-        <Text style={styles.text}>Repositories</Text>
-      </Link>
+      <ScrollView horizontal contentContainerStyle={styles.tabs}>
+        <Link to="/" component={Pressable} style={styles.tab}>
+          <Text style={styles.text}>Repositories</Text>
+        </Link>
 
-      <Link to="/signin" component={Pressable} style={styles.tab}>
-        <Text style={styles.text}>Sign in</Text>
-      </Link>
+        <Link to="/signin" component={Pressable} style={styles.tab}>
+          <Text style={styles.text}>Sign in</Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
