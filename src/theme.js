58adf292f75fced20d9844773743a60152b5e9c6
Student <student@sirafrica.org>
2026-09-15 15:21:04 +0300

    10. A platform-specific font

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -12,7 +14,11 @@ const theme = {
   },
 
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System',
+    }),
   },
 
   fontWeights: {
